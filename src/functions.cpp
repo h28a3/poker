97a79a5f0be268141353e7c4be53bdc094198385
h28a3 <at.h.s_24328@iCloud.com>
2024-08-06 12:13:59 +0900

    Add files via upload

---
+++ src/functions.cpp
@@ -0,0 +1,137 @@
+#include "header.h"
+
+void initialization() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    srand((unsigned int)time(NULL));
+    char set_card[4] = { 's','h','d','c' };
+    for (int i = 1;i <= 13; i++) {
+        for (char j : set_card) {
+            card c(i, j);
+            deck.push_back(c);
+        }
+    }
+}
+
+int score(vector<card> v) {
+    vector<int> set, sorted;
+    vector<char> set_c;
+    for (card i : v) {
+        if (i.num == 1) {
+            set.push_back(14);
+            sorted.push_back(14);
+        }
+        else {
+            set.push_back(i.num);
+            sorted.push_back(i.num);
+        }
+        set_c.push_back(i.c);
+    }
+    sort(sorted.begin(), sorted.end());
+
+    sort(set.begin(), set.end());
+    auto result1 = unique(set.begin(), set.end());
+    set.erase(result1, set.end());
+
+    sort(set_c.begin(), set_c.end());
+    auto result2 = unique(set_c.begin(), set_c.end());
+    set_c.erase(result2, set_c.end());
+
+    if (set.size() == 5 && sorted[0] == 10 && set_c.size() == 1) //ロイヤルストレートフラッシュ
+        return 9;
+    else if (set.size() == 5 && sorted[0] + 4 == sorted[4] && set_c.size() == 1) //ストレート・フラッシュ
+        return 8;
+    else if (sorted[0] == sorted[3] || sorted[1] == sorted[4]) //フォア・カード
+        return 7;
+    else if (set.size() == 2) //フルハウス
+        return 6;
+    else if (set_c.size() == 1) //フラッシュ
+        return 5;
+    else if (set.size() == 5 && sorted[0] + 4 == sorted[4]) //ストレート
+        return 4;
+    else if (sorted[0] == sorted[2] || sorted[1] == sorted[3] || sorted[2] == sorted[4]) //スリーカード
+        return 3;
+    else if (set.size() == 3) //ツウ・ペア
+        return 2;
+    else if (set.size() == 4) //ワン・ペア
+        return 1;
+    else
+        return 0;
+}
+
+vector<int> strategy(vector<card> v) {
+    vector<int> ans, set_num, sorted;
+    vector<char> e, set_c;
+    for (card i : v) {
+        if (i.num == 1) {
+            set_num.push_back(14);
+            sorted.push_back(14);
+        }
+        else {
+            set_num.push_back(i.num);
+            sorted.push_back(i.num);
+        }
+        e.push_back(i.c);
+        set_c.push_back(i.c);
+    }
+    unique(set_c.begin(), set_c.end());
+    sort(sorted.begin(), sorted.end());
+
+    if (set_c.size() == 1 || set_num.size() == 2) {
+        return ans;
+    }
+
+    if (set_c.size() == 2) {
+        for (char c : set_c) {
+            int cnt = count(e.begin(), e.end(), c);
+            if (cnt == 2) {
+                break;
+            }
+            else if (cnt == 1) {
+                for (int i = 0;i < 5;i++) {
+                    if (v[i].c == c) ans.push_back(i);
+                    break;
+                }
+            }
+        }
+        if (ans.size() > 0) {
+            return ans;
+        }
+    }
+
+    if (set_num.size() == 5) {
+        if (sorted[0] + 4 == sorted[4]) {
+            return ans;
+        }
+        else if (sorted[0] + 3 == sorted[3]) {
+            for (int i = 0;i < 5;i++) {
+                if (v[i].num == sorted[4])
+                    ans.push_back(i);
+            }
+        }
+        else if (sorted[1] + 3 == sorted[4]) {
+            for (int i = 0;i < 5;i++) {
+                if (v[i].num == sorted[0])
+                    ans.push_back(i);
+            }
+        }
+        if (ans.size() > 0) {
+            return ans;
+        }
+    }
+
+    if (set_num.size() < 5) {
+        for (int i : set_num) {
+            int cnt = count(set_num.begin(), set_num.end(), i);
+            if (cnt == 1) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+
+    for (int i = 0;i < 5;i++) {
+        ans.push_back(i);
+    }
+    return ans;
+}
---
+++ src/main.cpp
@@ -0,0 +1,197 @@
+﻿#include "header.h"
+
+vector<card> deck;
+
+int main(void) {
+    player players[N];
+    int player_score[N];
+    int judge = 1;
+    
+    while (judge > 0) {
+        initialization();
+        int bet = 1;
+        for (int i = 0;i < N;i++) {
+            vector<card> c = {};
+            for (int j = 0;j < 5;j++) {
+                int r = rand() % (52 - 5 * i - j);
+                c.push_back(deck[r]);
+                deck.erase(deck.begin() + r);
+            }
+            players[i].cards = c;
+        }
+
+        cout << "あなたのカードは，";
+        for (int i = 0;i < 5;i++) {
+            if (players[0].cards[i].c == 's') cout << "スペード";
+            if (players[0].cards[i].c == 'h') cout << "ハート";
+            if (players[0].cards[i].c == 'd') cout << "ダイヤ";
+            if (players[0].cards[i].c == 'c') cout << "クローバー";
+            cout << players[0].cards[i].num << "，";
+        }
+        cout << "です．\n";
+
+        for (int i = 0;i < N;i++) {
+            player_score[i] = score(players[i].cards);
+        }
+
+        int n = 0;
+        int flag = 0;
+
+        for (int i = 1;i < N;i++) {
+            if (player_score[i] > 2) {
+                if (bet >= players[i].chip) {
+                    cout << "COM" << i << "：オールイン\n";
+                    flag++;
+                    continue;
+                }
+                cout << "COM" << i << "：ビッド，";
+                int r = rand() % (MAX - bet) + 1;
+                r = min(r, players[i].chip);
+                cout << r << "枚追加\n";
+                bet += r;
+                n = i;
+                flag = 0;
+                break;
+            }
+            else {
+                cout << "COM" << i << "：パス\n";
+                flag++;
+            }
+        }
+
+        if (n > 0 && n < N - 1) {
+            for (int i = n + 1;i < N;i++) {
+                if (player_score[i] > 4) {
+                    if (bet >= players[i].chip) {
+                        cout << "COM" << i << "：オールイン\n";
+                        flag++;
+                        continue;
+                    }
+                    cout << "COM" << i << "：レイズ，";
+                    int r = rand() % 4 + 1;
+                    r = min(r, players[i].chip);
+                    cout << r << "枚追加\n";
+                    bet += r;
+                    flag = 0;
+                }
+                else {
+                    cout << "COM" << i << "：コール\n";
+                    flag++;
+                }
+            }
+        }
+
+        if (bet == 1) {
+            cout << "パスorビッド(0：パス，1：ビッド)";
+            int input;
+            cin >> input;
+            flag++;
+            if (input == 1) {
+                cout << "何枚追加しますか？(" << min(MAX, players[0].chip) - bet << "枚まで追加できます．)";
+                cin >> input;
+                bet += input;
+                n = 0;
+                flag = 0;
+            }
+            else if (input == 2)
+                continue;
+        }
+
+        while (flag < N - 1 && bet < MAX) {
+            n = (n + 1) % N;
+            if (player_score[n] > 5 && n != 0) {
+                if (bet >= players[n].chip) {
+                    cout << "COM" << n << "：オールイン\n";
+                    flag++;
+                    continue;
+                }
+                cout << "COM" << n << "：レイズ，";
+                int r = rand() % (MAX - bet) + 1;
+                r = min(r, players[n].chip);
+                cout << r << "枚追加\n";
+                bet += r;
+                flag = 0;
+            }
+            else if (n != 0) {
+                cout << "COM" << n << "：コール\n";
+                flag++;
+            }
+            else {
+                cout << "コールorレイズ(0：コール，1：レイズ)";
+                int input;
+                cin >> input;
+                flag++;
+                if (input == 1) {
+                    cout << "何枚追加しますか？(" << min(MAX, players[0].chip) - bet << "枚まで追加できます．)";
+                    cin >> input;
+                    bet += input;
+                    flag = 0;
+                }
+            }
+        }
+
+        bet = 0;
+
+        for (int i = 0;i < N;i++) {
+            int a = min(bet, players[i].chip);
+            players[i].chip -= a;
+            bet += a;
+        }
+
+        int rest = 52 - 5 * N;
+        cout << "何枚カードを交換しますか？";
+        cin >> n;
+        rest -= n;
+        if (n > 0) {
+            cout << "何枚目を交換しますか？(空白区切りで入力)";
+            for (int i = 0;i < n;i++) {
+                int rep;
+                cin >> rep;
+                int r = rand() % (32 - i);
+                players[0].cards[rep-1] = deck[r];
+                deck.erase(deck.begin() + r);
+            }
+        }
+
+        cout << "あなたのカードは，";
+        for (int i = 0;i < 5;i++) {
+            if (players[0].cards[i].c == 's') cout << "スペード";
+            if (players[0].cards[i].c == 'h') cout << "ハート";
+            if (players[0].cards[i].c == 'd') cout << "ダイヤ";
+            if (players[0].cards[i].c == 'c') cout << "クローバー";
+            cout << players[0].cards[i].num << "，";
+        }
+        cout << "です．\n";
+
+        for (int i = 1;i < N;i++) {
+            vector<int> rep = strategy(players[i].cards);
+            for (int j : rep) {
+                int r = rand() % (rest);
+                players[0].cards[j] = deck[r];
+                deck.erase(deck.begin() + r);
+                rest--;
+            }
+            cout << "COM" << i << "：" << rep.size() << "枚交換\n";
+        }
+
+        for (int i = 0;i < N;i++) {
+            player_score[i] = score(players[i].cards);
+        }
+        int winner= *max_element(begin(player_score), end(player_score));
+        
+        judge = 1;
+        for (int i = 0;i < N;i++) {
+            if (player_score[i] == winner) {
+                players[i].chip += bet;
+                if (i == 0) {
+                    cout << "プレイヤーの勝利\n";
+                }
+                else {
+                    cout << "COM" << i << "の勝利\n";
+                }
+            }
+            judge *= players[i].chip;
+        }
+        break;
+    }
+}
