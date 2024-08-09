62ebc4875dcaffa6eefc23462ebb996eb2256e11
h28a3 <at.h.s_24328@iCloud.com>
2024-08-09 22:41:22 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
-﻿#include "header.h"
+#include "header.h"
 
-vector<card> deck;
+vector<card> deck; //山札
 
 int main(void) {
-    player players[N];
-    int player_score[N];
-    int judge = 1;
+    player players[N]; //プレイヤーとCOMのカード，チップ記憶
+    int player_score[N]; //プレイヤーとCOMのスコア(score関数参照)
+    int judge = 1; //ループ判定用変数
     
-    while (judge > 0) {
-        initialization();
-        int bet = 1;
-        for (int i = 0;i < N;i++) {
+    while (judge > 0) { //誰かのチップがなくなるまで無限ループ
+        initialization(); //山札生成
+        int bet = 1; //場に一人が賭けているチップ数
+        for (int i = 0;i < N;i++) { //一人に五枚ずつ配る
             vector<card> c = {};
             for (int j = 0;j < 5;j++) {
                 int r = rand() % (52 - 5 * i - j);
@@ -20,25 +20,17 @@ int main(void) {
             players[i].cards = c;
         }
 
-        cout << "あなたのカードは，";
-        for (int i = 0;i < 5;i++) {
-            if (players[0].cards[i].c == 's') cout << "スペード";
-            if (players[0].cards[i].c == 'h') cout << "ハート";
-            if (players[0].cards[i].c == 'd') cout << "ダイヤ";
-            if (players[0].cards[i].c == 'c') cout << "クローバー";
-            cout << players[0].cards[i].num << "，";
-        }
-        cout << "です．\n";
+        show_card(0, players[0].cards); //プレイヤーのカード出力
 
         for (int i = 0;i < N;i++) {
-            player_score[i] = score(players[i].cards);
+            player_score[i] = score(players[i].cards); //全員のスコア計算
         }
 
         int n = 0;
         int flag = 0;
 
         for (int i = 1;i < N;i++) {
-            if (player_score[i] > 2) {
+            if (player_score[i] > 2) { //COMはスリーカードより良い役のときはビッド
                 if (bet >= players[i].chip) {
                     cout << "COM" << i << "：オールイン\n";
                     flag++;
@@ -61,7 +53,8 @@ int main(void) {
 
         if (n > 0 && n < N - 1) {
             for (int i = n + 1;i < N;i++) {
-                if (player_score[i] > 4) {
+                n = i;
+                if (player_score[i] > 4) { //COMはフラッシュより良い役のときはレイズ
                     if (bet >= players[i].chip) {
                         cout << "COM" << i << "：オールイン\n";
                         flag++;
@@ -81,7 +74,7 @@ int main(void) {
             }
         }
 
-        if (bet == 1) {
+        if (bet == 1) { //COMが誰もビッドしなかったらプレイヤーはパスかビッドを選択
             cout << "パスorビッド(0：パス，1：ビッド)";
             int input;
             cin >> input;
@@ -130,19 +123,26 @@ int main(void) {
             }
         }
 
-        bet = 0;
+        int sum = 0; //場に出ているチップ
 
         for (int i = 0;i < N;i++) {
             int a = min(bet, players[i].chip);
+            sum += a;
             players[i].chip -= a;
-            bet += a;
         }
 
-        int rest = 52 - 5 * N;
+        int rest = 52 - 5 * N; //山札の残り枚数
         cout << "何枚カードを交換しますか？";
         cin >> n;
         rest -= n;
-        if (n > 0) {
+        if (n == 5) { //5枚全て交換するとき
+            for (int i = 0;i < 5;i++) {
+                int r = rand() % (32 - i);
+                players[0].cards[i] = deck[r];
+                deck.erase(deck.begin() + r);
+            }
+        }
+        else if (n > 0) {
             cout << "何枚目を交換しますか？(空白区切りで入力)";
             for (int i = 0;i < n;i++) {
                 int rep;
@@ -153,44 +153,101 @@ int main(void) {
             }
         }
 
-        cout << "あなたのカードは，";
-        for (int i = 0;i < 5;i++) {
-            if (players[0].cards[i].c == 's') cout << "スペード";
-            if (players[0].cards[i].c == 'h') cout << "ハート";
-            if (players[0].cards[i].c == 'd') cout << "ダイヤ";
-            if (players[0].cards[i].c == 'c') cout << "クローバー";
-            cout << players[0].cards[i].num << "，";
-        }
-        cout << "です．\n";
+        show_card(0, players[0].cards); //交換後のプレイヤーのカード出力
 
         for (int i = 1;i < N;i++) {
-            vector<int> rep = strategy(players[i].cards);
+            vector<int> rep = strategy(players[i].cards); //COMが交換するカード
             for (int j : rep) {
                 int r = rand() % (rest);
-                players[0].cards[j] = deck[r];
+                players[i].cards[j] = deck[r];
                 deck.erase(deck.begin() + r);
                 rest--;
+                if (rest == 0) {
+                    initialization();
+                }
             }
             cout << "COM" << i << "：" << rep.size() << "枚交換\n";
         }
 
+        vector<int> card_sum;
         for (int i = 0;i < N;i++) {
             player_score[i] = score(players[i].cards);
-        }
-        int winner= *max_element(begin(player_score), end(player_score));
-        
-        judge = 1;
-        for (int i = 0;i < N;i++) {
-            if (player_score[i] == winner) {
-                players[i].chip += bet;
-                if (i == 0) {
-                    cout << "プレイヤーの勝利\n";
+            int a = 0;
+            for (int j = 0;j < 5;j++) {
+                if (players[i].cards[j].num == 1) {
+                    a += 14;
                 }
                 else {
-                    cout << "COM" << i << "の勝利\n";
+                    a += players[i].cards[j].num;
                 }
             }
-            judge *= players[i].chip;
+            card_sum.push_back(a);
+            if (i > 0) {
+                show_card(i, players[i].cards); //交換後のCOMのカード出力
+            }
         }
+        int winner = *max_element(begin(player_score), end(player_score));
+        
+        judge = 1;
+
+        if (count(begin(player_score), end(player_score), winner) == 1) { //スコアで一人勝ちの場合はそのまま場のチップ総取り
+            for (int i = 0;i < N;i++) {
+                if (player_score[i] == winner) {
+                    players[i].chip += sum;
+                    if (i == 0) {
+                        cout << "プレイヤーの勝利\n";
+                    }
+                    else {
+                        cout << "COM" << i << "の勝利\n";
+                    }
+                }
+                judge *= players[i].chip;
+            }
+        }
+        else { //スコアで同点の人がいる場合，カードの総和で勝負(ただしポーカーでは1が一番強いので，1は14としてカウント)
+            vector<int> winners,tmp;
+            for (int i = 0;i < N;i++) {
+                if (player_score[i] == winner) {
+                    winners.push_back(i);
+                }
+            }
+            for (int i : winners) {
+                tmp.push_back(card_sum[i]);
+            }
+            int max = *max_element(tmp.begin(), tmp.end());
+            if (count(tmp.begin(), tmp.end(), max) == 1) { //カードの総和で一人勝ちの場合はそのまま場のチップ総取り
+                for (int i : winners) {
+                    if (card_sum[i] == max) {
+                        players[i].chip += sum;
+                        if (i == 0) {
+                            cout << "プレイヤーの勝利\n";
+                        }
+                        else {
+                            cout << "COM" << i << "の勝利\n";
+                        }
+                    }
+                }
+            }
+            else { //カードの総和で同点の人がいる場合はそのまま場のチップを山分け
+                int n = count(tmp.begin(), tmp.end(), max);
+                for (int i : winners) {
+                    if (card_sum[i] == max) {
+                        players[i].chip += sum / n;
+                        if (i == 0) {
+                            cout << "プレイヤーと";
+                        }
+                        else {
+                            cout << "COM" << i << "と";
+                        }
+                    }
+                }
+                cout << "の勝利\n";
+            }
+        }
+        for (int i = 0;i < N;i++) {
+            show_chip(i, players[i].chip);
+        }
+        cout << "\n";
     }
+    return 0;
 }
