0cd3f0b6168d3460cffde93fda28eaf402756ba9
h28a3 <at.h.s_24328@iCloud.com>
2024-08-11 09:08:50 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "header.h"
 
 vector<card> deck; //山札
+int player_score[N]; //プレイヤーとCOMのスコア(score関数参照)
 
 int main(void) {
-    player players[N]; //プレイヤーとCOMのカード，チップ記憶
-    int player_score[N]; //プレイヤーとCOMのスコア(score関数参照)
     int judge = 1; //ループ判定用変数
+    player players[N]; //プレイヤーとCOMのカード，チップ記憶
     
     while (judge > 0) { //誰かのチップがなくなるまで無限ループ
         initialization(); //山札生成
@@ -29,27 +29,7 @@ int main(void) {
         int n = 0;
         int flag = 0;
 
-        for (int i = 1;i < N;i++) {
-            if (player_score[i] > 2) { //COMはスリーカードより良い役のときはビッド
-                if (bet >= players[i].chip) {
-                    cout << "COM" << i << "：オールイン\n";
-                    flag++;
-                    continue;
-                }
-                cout << "COM" << i << "：ビッド，";
-                int r = rand() % (MAX - bet) + 1;
-                r = min(r, players[i].chip);
-                cout << r << "枚追加\n";
-                bet += r;
-                n = i;
-                flag = 0;
-                break;
-            }
-            else {
-                cout << "COM" << i << "：パス\n";
-                flag++;
-            }
-        }
+        bid_or_pass(bet, flag, n, players);
 
         if (n > 0 && n < N - 1) {
             for (int i = n + 1;i < N;i++) {
@@ -72,22 +52,6 @@ int main(void) {
                     flag++;
                 }
             }
-        }
-
-        if (bet == 1) { //COMが誰もビッドしなかったらプレイヤーはパスかビッドを選択
-            cout << "パスorビッド(0：パス，1：ビッド)";
-            int input;
-            cin >> input;
-            flag++;
-            if (input == 1) {
-                cout << "何枚追加しますか？(" << min(MAX, players[0].chip) - bet << "枚まで追加できます．)";
-                cin >> input;
-                bet += input;
-                n = 0;
-                flag = 0;
-            }
-            else if (input == 2)
-                continue;
         }
 
         while (flag < N - 1 && bet < MAX) {
@@ -169,6 +133,10 @@ int main(void) {
             cout << "COM" << i << "：" << rep.size() << "枚交換\n";
         }
 
+        n = 0;
+        flag = 0;
+        bid_or_pass(bet, flag, n, players);
+
         vector<int> card_sum;
         for (int i = 0;i < N;i++) {
             player_score[i] = score(players[i].cards);
@@ -187,8 +155,6 @@ int main(void) {
             }
         }
         int winner = *max_element(begin(player_score), end(player_score));
-        
-        judge = 1;
 
         if (count(begin(player_score), end(player_score), winner) == 1) { //スコアで一人勝ちの場合はそのまま場のチップ総取り
             for (int i = 0;i < N;i++) {
@@ -201,7 +167,6 @@ int main(void) {
                         cout << "COM" << i << "の勝利\n";
                     }
                 }
-                judge *= players[i].chip;
             }
         }
         else { //スコアで同点の人がいる場合，カードの総和で勝負(ただしポーカーでは1が一番強いので，1は14としてカウント)
@@ -248,6 +213,10 @@ int main(void) {
             show_chip(i, players[i].chip);
         }
         cout << "\n";
+        judge = 1;
+        for(int i=0;i<N;i++){
+            judge *= players[i].chip;
+        }
     }
     return 0;
 }
